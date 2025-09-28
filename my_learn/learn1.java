9b033d56ca463b1c44d40b7343022173c84394fd
fallwjj <2510353131@qq.com>
2025-09-28 12:43:04 +0800

    9.28更新

---
+++ my_learn/learn1.java
@@ -0,0 +1,7 @@
+public class Learn1 {
+    public static void main(String[] args) {
+        System.out.println('a');
+        System.out.println("你好，Java世界！");
+        System.out.print("m");// ice 未定义，已注释
+    }
+}
